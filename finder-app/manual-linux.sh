7f939907536dde340fca8ac822265145640c23d4
vidhya <vidhya@Ubuntu.myguest.virtualbox.org>
2023-09-17 21:36:35 -0600

    removed dead code

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
+# Modified: Vidhya. PL
+# Date: 09/17/2023
 
 set -e
 set -u
@@ -23,6 +25,12 @@ fi
 
 mkdir -p ${OUTDIR}
 
+#Checking if directory creation failed
+if [ ! -d ${OUTDIR}]; then
+   	echo Output directory does not exist
+   	exit 1      #Failure Operation
+fi
+
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
@@ -33,14 +41,14 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
-   # git apply /home/vidhya/Downloads/dtc-multiple-definition.patch
 
     # TODO: Add your kernel build steps here
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    #from lecture slides    
+     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+     make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
 fi
 
 echo "Adding the Image in outdir"
@@ -49,20 +57,15 @@ echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
 # TODO: Create necessary base directories
-mkdir -p ${OUTDIR}/rootfs
-cd ${OUTDIR}/rootfs
-mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-mkdir -p usr/bin usr/lib usr/sbin
-mkdir -p var/log
-
-#mkdir -p rootfs/{bin,dev,etc,home,lib,lib64,proc,sbin,sys,tmp,usr,var}
-#mkdir -p rootfs/usr/{bin,lib,sbin}
-#mkdir -p rootfs/var/log
+#from lecture slides
+mkdir -p ${OUTDIR}/rootfs && cd ${OUTDIR}/rootfs
+echo Creating the neccessary base directories
+mkdir -p {bin,dev,etc,home,lib,lib64,proc,sbin,sys,tmp,usr,var} usr/{bin,lib,sbin} var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -70,18 +73,20 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
+    
     # TODO:  Configure busybox
+    #from lecture slides
+    echo Configuration of Busybox
     make distclean
     make defconfig
 else
     cd busybox
 fi
 
-
-
 # TODO: Make and install busybox
-#make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}https://github.com/cu-ecen-aeld/assignments-3-and-later-vidhyapalaniappan
-make -j4 CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+#from lecture slides
+echo Making and installing Busybox
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
@@ -89,31 +94,23 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-#cp -L _install/lib/* rootfs/lib/
-#cp -L _install/bin/busybox rootfs/bin/
+#obtain from  ChatGPT at https://chat.openai.com/ with prompts including "Add library dependencies to rootfs"
+echo Adding library dependencies to rootfs
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 echo "${SYSROOT}"
-#cp -f $SYSROOT/lib/ld-linux-aarch64.so.1 lib
-#cp -f $SYSROOT/lib64/libm.so.6 lib64
-#cp -f $SYSROOT/lib64/libresolv.so.2 lib64
-#cp -f $SYSROOT/lib64/libc.so.6 lib64
-
-cp -a $SYSROOT/lib/ld-linux-aarch64.so.1 lib
-cp -a $SYSROOT/lib64/ld-2.31.so lib64
-cp -a $SYSROOT/lib64/libm.so.6 lib64
-cp -a $SYSROOT/lib64/libresolv.so.2 lib64
-cp -a $SYSROOT/lib64/libc.so.6 lib64
-cp -a $SYSROOT/lib64/libm-2.31.so lib64
-cp -a $SYSROOT/lib64/libresolv-2.31.so lib64
-cp -a $SYSROOT/lib64/libc-2.31.so lib64
+cp -f $SYSROOT/lib/ld-linux-aarch64.so.1 lib
+cp -f $SYSROOT/lib64/libm.so.6 lib64
+cp -f $SYSROOT/lib64/libresolv.so.2 lib64
+cp -f $SYSROOT/lib64/libc.so.6 lib64
 
 # TODO: Make device nodes
-#sudo mknod -m 666 rootfs/dev/null c 1 3
-#sudo mknod -m 666 rootfs/dev/tty c 5 0
+#from lecture slides
+echo Making Device nodes
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+echo Cleaning and building the writer utility
 #cd "$FINDER_APP_DIR/writer"
 cd ${FINDER_APP_DIR}
 make clean
@@ -121,19 +118,25 @@ make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo Copying scripts and executables to the /home directory
+cp -f ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
+cp -f ${FINDER_APP_DIR}/conf/username.txt ${OUTDIR}/rootfs/home
 cp -f ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home
 cp -f ${FINDER_APP_DIR}/conf/ -r ${OUTDIR}/rootfs/home
-cp -f ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
 cp -f ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
 cp -f ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home
 cp -f ${FINDER_APP_DIR}/writer.c ${OUTDIR}/rootfs/home
-cp -f ${FINDER_APP_DIR}/conf/username.txt ${OUTDIR}/rootfs/home
+cp -f ${FINDER_APP_DIR}/conf/assignment.txt ${OUTDIR}/rootfs/home
 
 # TODO: Chown the root directory
+#from lecture slides
+echo Chowning the root directory
 cd ${OUTDIR}/rootfs
 sudo chown -R root:root *
+
 # TODO: Create initramfs.cpio.gz
-#find . | cpio -H newc -ov --owner root:root | gzip -9 > ../initramfs.cpio.gz
+#from lecture slides
+echo Creating initramfs.cpio.gz
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 cd ..
 echo "$(pwd)"
