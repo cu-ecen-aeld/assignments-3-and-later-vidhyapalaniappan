d03b4fea20feee27435c1b8d35b4a1265cb54860
vidhyapalaniappan <vpa4058@colorado.edu>
2023-11-07 13:26:17 -0700

    for each added

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -204,7 +204,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count, loff
 
 loff_t aesd_llseek(struct file *filp, loff_t offset, int whence)
 {
-    struct aesd_buffer_entry *entry = NULL;
+    struct aesd_buffer_entry *buff_entry = NULL;
     struct aesd_dev *dev = filp->private_data;
     loff_t seek_offset = 0;
     loff_t seek_size = 0;
@@ -218,9 +218,9 @@ loff_t aesd_llseek(struct file *filp, loff_t offset, int whence)
     {
         return -ERESTARTSYS;
     }
-    AESD_CIRCULAR_BUFFER_FOREACH(entry,&aesd_device.cbuff,i)
+    AESD_CIRCULAR_BUFFER_FOREACH(buff_entry,&aesd_device.cbuff,i)
     {
-        seek_size += entry->size;
+        seek_size += buff_entry->size;
     }
     mutex_unlock(&dev->mutex_lock);
     seek_offset = fixed_size_llseek(filp, offset, whence, seek_size);
@@ -228,10 +228,10 @@ loff_t aesd_llseek(struct file *filp, loff_t offset, int whence)
 
 }
 
-static long aesd_adjust_file_offset(struct file *filp, unsigned int write_cmd, unsigned int write_cmd_offset)
+static long file_offset_move(struct file *filp, unsigned int write_cmd, unsigned int write_cmd_offset)
 {
     struct aesd_dev *dev = filp->private_data;
-    struct aesd_buffer_entry *entry = NULL;
+    struct aesd_buffer_entry *buff_entry = NULL;
     long retval = 0;
     uint8_t i = 0;
 
@@ -243,22 +243,20 @@ static long aesd_adjust_file_offset(struct file *filp, unsigned int write_cmd, u
     {
         return -ERESTARTSYS;
     }
-    AESD_CIRCULAR_BUFFER_FOREACH(entry,&aesd_device.cbuff,i)
+    AESD_CIRCULAR_BUFFER_FOREACH(buff_entry,&aesd_device.cbuff,i)
     {
 
     }
     if ((write_cmd > AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) || (write_cmd > i) || (write_cmd_offset >= dev->cbuff.entry[write_cmd].size))
     {
-        retval = -EINVAL;
-        goto exit;
+    	mutex_unlock(&device->mutex_lock);
+    	return -EINVAL;
     }
     for (i = 0; i < write_cmd; i++)
     {
         filp->f_pos += dev->cbuff.entry[i].size;
     }
     filp->f_pos += write_cmd_offset;
-
-exit:
     mutex_unlock(&dev->mutex_lock);
     return retval;
 }
@@ -290,7 +288,7 @@ long aesd_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
         }
         else
         {
-          retval = aesd_adjust_file_offset(filp, seeker.write_cmd, seeker.write_cmd_offset);
+          retval = file_offset_move(filp, seeker.write_cmd, seeker.write_cmd_offset);
         }
         break;
 
